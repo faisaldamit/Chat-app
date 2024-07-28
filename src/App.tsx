0415e9ed0f85b8d66e48729df004d8ad9a6ad1b6
faisaldamit <fbabtayn@gmail.com>
2024-07-28 18:00:33 +0100

    fiest com 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,51 @@ import profilePictureDefault from "../public/UI/images/profile-picture.webp";
 import {
   faThumbTack,
   faMagnifyingGlass,
+  faMicrophone,
+  faBars,
+  faPaperclip,
+  faFaceSmile,
+  faPaperPlane,
 } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className=" w-full h-dvh ">
+    <div className=" w-full h-full p-2 grid grid-cols-4 gap-4 max-[850]:grid-cols-3 max-[850]:grid ">
       <div
-        className="sm:w-1/3 w-1/2 h-dvh rounded-r-2xl 
-      box-border bg-gray-700/10 z-999 "
+        className="col-span-1 w-full rounded-2xl 
+          box-border bg-gray-700/10 z-999"
       >
-        <div className="flex last:ml-auto md:shrink-0 max-[600px]:justify-between">
-          <img
-            src={profilePictureDefault}
-            className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-            alt="profile"
-          />
-          <div className="w-4/5 p-4 max-[600px]:hidden  ">
-            <p className="text-lg font-semibold ">Eric Ten Hag</p>
+        <div className="flex last:ml-auto rounded-t-2xl border-4 border-b-slate-950 ">
+          <div className="grid place-items-center w-1/6 m-2 max-[740px]:w-1/5 ">
+            <img
+              src={profilePictureDefault}
+              className="rounded-full w-full "
+              alt="profile"
+            />
+          </div>
+          <div className="w-4/5 p-4 max-[900px]:w-1/2 pt-6  ">
+            <p className="text-sm font-semibold ">Eric Ten Hag</p>
             <a href="">
               <p className="text-sm">Info account</p>
             </a>
           </div>
-          <div
-            className="bg-gray-800/10 rounded-full 
-                          h-8 w-9 m-5 mt-7 relative right-30
-                          hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 "
-          >
-            <button type="button" className=" rounded-full static ">
+
+          <div className="grid place-items-center">
+            <button
+              type="button"
+              className="bg-gray-800/10 rounded-full relative
+                          h-12 w-12 m-5 hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50"
+            >
               <FontAwesomeIcon
                 icon={faMagnifyingGlass}
-                className="absolute bottom-1/4 left-1/4"
+                className="absolute bottom-1/3 left-1/3"
               />
             </button>
           </div>
         </div>
 
-        <div className="rounded-r bg-red-100 h-auto overflow-auto scrollbar:hidden">
+        <div className="rounded-2xl bg-red-100 gap-1 ">
           <div className=" flex  justify-between ">
             <div className="flex-none w-15 rounded-full p-2">
               Pinned messages
@@ -50,215 +58,212 @@ function App() {
               <FontAwesomeIcon icon={faThumbTack} className="w-10 mt-3" />
             </div>
           </div>
+
           <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+            className=" flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
                           focus:ring focus:ring-slate-50 cursor-pointer"
           >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
+            <div className="grid place-items-center w-1/6 h-full m-auto max-[600px]:w-1/4">
+              <img
+                src={profilePictureDefault}
+                className=" rounded-full w-full h-full "
+                alt="profile"
+              />
             </div>
-          </button>
-          
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
+            <div className="w-4/5 p-4 pt-7 max-[500px]:hidden text-left ">
+              <p className="text-lg font-semibold max[740]:text-xl">
+                Eric Ten Hag
+              </p>
               <a href="">
                 <p className="text-sm">Info account</p>
               </a>
             </div>
           </button>
           <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+            className=" flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
                           focus:ring focus:ring-slate-50 cursor-pointer"
           >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
+            <div className="grid place-items-center w-1/6 h-full m-auto max-[600px]:w-1/4">
+              <img
+                src={profilePictureDefault}
+                className="  rounded-full w-full h-full "
+                alt="profile"
+              />
+            </div>
+            <div className="w-4/5 p-4 pt-7 max-[500px]:hidden text-left ">
+              <p className="text-lg font-semibold max[740]:text-xl">
+                Eric Ten Hag
+              </p>
               <a href="">
                 <p className="text-sm">Info account</p>
               </a>
             </div>
           </button>
           <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+            className=" flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
                           focus:ring focus:ring-slate-50 cursor-pointer"
           >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
+            <div className="grid place-items-center w-1/6 h-full m-auto max-[600px]:w-1/4">
+              <img
+                src={profilePictureDefault}
+                className="  rounded-full w-full h-full "
+                alt="profile"
+              />
+            </div>
+            <div className="w-4/5 p-4 pt-7 max-[500px]:hidden text-left ">
+              <p className="text-lg font-semibold max[740]:text-xl">
+                Eric Ten Hag
+              </p>
+              <a href="">
+                <p className="text-sm">Info account</p>
+              </a>
+            </div>
+          </button>
+
+          <button
+            className=" flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50 cursor-pointer"
+          >
+            <div className="grid place-items-center w-1/6 h-full m-auto max-[600px]:w-1/4">
+              <img
+                src={profilePictureDefault}
+                className="  rounded-full w-full h-full "
+                alt="profile"
+              />
+            </div>
+            <div className="w-4/5 p-4 pt-7 max-[500px]:hidden text-left ">
+              <p className="text-lg font-semibold max[740]:text-xl">
+                Eric Ten Hag
+              </p>
+              <a href="">
+                <p className="text-sm">Info account</p>
+              </a>
+            </div>
+          </button>
+
+          <button
+            className=" flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50 cursor-pointer"
+          >
+            <div className="grid place-items-center w-1/6 h-full m-auto max-[600px]:w-1/4">
+              <img
+                src={profilePictureDefault}
+                className="  rounded-full w-full h-full "
+                alt="profile"
+              />
+            </div>
+            <div className="w-4/5 p-4 pt-7 max-[500px]:hidden text-left ">
+              <p className="text-lg font-semibold max[740]:text-xl">
+                Eric Ten Hag
+              </p>
               <a href="">
                 <p className="text-sm">Info account</p>
               </a>
             </div>
           </button>
           <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+            className=" flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
                           focus:ring focus:ring-slate-50 cursor-pointer"
           >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
+            <div className="grid place-items-center w-1/6 h-full m-auto max-[600px]:w-1/4">
+              <img
+                src={profilePictureDefault}
+                className="  rounded-full w-full h-full "
+                alt="profile"
+              />
+            </div>
+            <div className="w-4/5 p-4 pt-7 max-[500px]:hidden text-left ">
+              <p className="text-lg font-semibold max[740]:text-xl">
+                Eric Ten Hag
+              </p>
               <a href="">
                 <p className="text-sm">Info account</p>
               </a>
             </div>
           </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
+        </div>
+      </div>
+      <div className=" col-span-3 flex flex-col justify-between bg-green-200 rounded-2xl z-999 ">
+        <div className="flex flex-row justify-start last:ml-auto border-4 border-b-slate-950 rounded-t-2xl bg-slate-200">
+          <div className="grid place-items-center w-20 ml-4 h-full">
             <img
               src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
+              className="  rounded-full w-full h-full "
               alt="profile"
             />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
+          </div>
+          <div className="w-auto m-auto ml-0">
+            <p className="m-auto ml-4 text-lg font-semibold max[740]:text-xl">
+              Eric Ten Hag
+            </p>
+            <p className="m-auto ml-6 text-sm ">Eric Ten Hag</p>
+          </div>
+          <div className="grid place-items-center">
+            <button
+              type="button"
+              className="bg-gray-800/10 rounded-full relative
+                          h-12 w-12 m-5 hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50"
+            >
+              <FontAwesomeIcon
+                icon={faBars}
+                className="absolute bottom-1/3 left-1/3"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-start border-4 border-t-slate-950 rounded-b-2xl bg-slate-200">
+          <div className="grid place-items-center">
+            <button
+              type="button"
+              className="bg-gray-800/10 rounded relative
+                          h-12 w-12 m-3 mr-1 hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50"
+            >
+              <FontAwesomeIcon icon={faFaceSmile} />
+            </button>
+          </div>
+          <div className="grid place-items-center">
+            <button
+              type="button"
+              className="bg-gray-800/10 rounded relative
+                          h-12 w-12 m-3 ml-1 hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50"
+            >
+              <FontAwesomeIcon icon={faPaperclip} />
+            </button>
+          </div>
+          <div className="grid place-items-start mt-4 w-full ml-0 h-full">
+            <input
+              type="text"
+              placeholder="Type a message"
+              className="bg-gray-700 w-5/6 text-gray-200 rounded-xl px-4 py-2 pl-auto flex-grow focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none overflow-hidden min-h-[44px] max-h-[200px]"
             />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
-          <button
-            className="flex max-[600px]:justify-between hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
-                          focus:ring focus:ring-slate-50 cursor-pointer"
-          >
-            <img
-              src={profilePictureDefault}
-              className="rounded-full w-1/6 p-4 max-[600px]:w-1/3 max-[600px]:h-full"
-              alt="profile"
-            />
-            <div className="w-4/5 p-4 max-[600px]:hidden text-left ">
-              <p className="text-lg font-semibold ">Eric Ten Hag</p>
-              <a href="">
-                <p className="text-sm">Info account</p>
-              </a>
-            </div>
-          </button>
+          </div>
+          <div className="w-auto m-auto ml-0">
+            <form action="text" className=""></form>
+          </div>
+          <div className="grid place-items-center">
+            <button
+              type="button"
+              className="bg-gray-800/10 rounded relative
+                          h-12 w-12 m-3 mr-0 hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50"
+            >
+              <FontAwesomeIcon icon={faMicrophone} />
+            </button>
+          </div>
+          <div className="grid place-items-center">
+            <button
+              type="button"
+              className="bg-gray-800/10 rounded relative
+                          h-12 w-12 m-3 ml-1 hover:bg-slate-50 active:bg-slate-50 focus:outline-none 
+                          focus:ring focus:ring-slate-50"
+            >
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
